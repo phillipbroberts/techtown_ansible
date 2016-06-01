8a0d46b1a22bafb290987fc7f5b5813703e5fb9a
Mitchell Smith <mjs@mjsprojects.net>
2016-06-01 16:31:58 -0700

    fixing all projects with windows private key work around

--- amazon-ec2/install_ansible.sh
+++ amazon-ec2/install_ansible.sh
@@ -6,3 +6,7 @@ sudo yum --quiet --assumeyes  install epel-release
 echo "Installing Ansible"
 sudo yum --quiet --assumeyes  install ansible
 sudo rm -rf /etc/ansible/*
+
+echo "Installing SSH private key"
+cp /vagrant/.private_key /home/vagrant/.ssh/id_rsa
+chmod 600 /home/vagrant/.ssh/id_rsa
--- kubernetes/install_ansible.sh
+++ kubernetes/install_ansible.sh
@@ -4,5 +4,9 @@ echo "Installing EPEL repository"
 sudo yum --quiet --assumeyes  install epel-release
 
 echo "Installing Ansible"
-sudo yum --quiet --assumeyes  install ansible python-netaddr
+sudo yum --quiet --assumeyes  install ansible
 sudo rm -rf /etc/ansible/*
+
+echo "Installing SSH private key"
+cp /vagrant/.private_key /home/vagrant/.ssh/id_rsa
+chmod 600 /home/vagrant/.ssh/id_rsa
