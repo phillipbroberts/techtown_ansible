743b1a613b49dbc1969710c713f4d6f4d941b51c
Mitchell Smith <mjs@mjsprojects.net>
2016-06-01 16:41:11 -0700

    fixing file ownership problem

--- rocket-chat/install_ansible.sh
+++ rocket-chat/install_ansible.sh
@@ -10,3 +10,4 @@ sudo rm -rf /etc/ansible/*
 echo "Installing SSH private key"
 cp /vagrant/.private_key /home/vagrant/.ssh/id_rsa
 chmod 600 /home/vagrant/.ssh/id_rsa
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa
