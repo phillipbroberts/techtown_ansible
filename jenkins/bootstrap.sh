7335183424b9ff160878908f4547848c28d71f43
Mitchell Smith <mjs@mjsprojects.net>
2016-06-02 21:11:38 -0700

    fixing some issues in the testing framework

--- jenkins/bootstrap.sh
+++ jenkins/bootstrap.sh
@@ -7,6 +7,8 @@ apt-get -qqy dist-upgrade
 echo "Installing LXC"
 cp /vagrant/conf/lxc-net /etc/default/
 apt-get -qqy install lxc lxc-templates lxctl debootstrap
+echo "Installing git"
+apt-get -qqy install git
 echo "Installing Java"
 apt-get -qqy install default-jre-headless openjdk-8-jre-headless
 echo "Installing Jenkins"
@@ -14,9 +16,11 @@ wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
 cp /vagrant/conf/jenkins.list /etc/apt/sources.list.d/
 apt-get -qq update
 apt-get -qqy install jenkins
-echo "Updating sudoers to give jenkins access to lxc commands"
+echo "Adding jenkins to sudo group"
+adduser jenkins sudo
 cp /vagrant/conf/sudoers /etc/sudoers
 chmod 440 /etc/sudoers
+chown root:root /etc/sudoers
 echo "Copying lxc helper scripts for jenkins"
 cp /vagrant/scripts/* /usr/local/bin
 chmod 755 /usr/local/bin/build_base_containers /usr/local/bin/spinup_lxc /usr/local/bin/run_lxc /usr/local/bin/teardown_lxc
